2b4a4359400b176e585a1ad73437b3741c83bb2c
y-bash <ybash.jp@gmail.com>
2020-10-05 16:00:51 +0900

    add lang.go

---
+++ lang_test.go
@@ -0,0 +1,49 @@
+package trans
+
+import (
+	"strings"
+	"testing"
+)
+
+type FindLangTest struct {
+	in   string
+	code string
+	name string
+	err  string
+}
+
+var findlangtests = []FindLangTest{
+	0: {"ja", "ja", "Japanese", ""},
+	1: {"JA", "ja", "Japanese", ""},
+	2: {"jap", "ja", "Japanese", ""},
+	3: {"japan", "ja", "Japanese", ""},
+	4: {"jAPANESE", "ja", "Japanese", ""},
+	5: {"en", "en", "English", ""},
+	6: {"en", "en", "English", ""},
+	7: {"frisia", "fy", "Western Frisian", ""},
+	8: {"zz", "", "", "not found"},
+}
+
+func TestFindLang(t *testing.T) {
+	for i, tt := range findlangtests {
+		code, name, err := findLang(tt.in)
+		if err != nil {
+			if tt.err == "" {
+				t.Errorf("#%d have error: %s, want error: none", i, err.Error())
+				continue
+			}
+			if !strings.Contains(err.Error(), tt.err) {
+				t.Errorf("#%d have error: %s, want error: %s", i, err.Error(), tt.err)
+			}
+			continue
+		}
+		if tt.err != "" {
+			t.Errorf("#%d have error: none, want error: %s", i, tt.err)
+			continue
+		}
+		if code != tt.code || name != tt.name {
+			t.Errorf("#%d findLang(%q) = (%q, %q, nil)  want: (%q, %q, nil)",
+				i, tt.in, code, name, tt.code, tt.name)
+		}
+	}
+}
